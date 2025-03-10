e198c084af01628a85516aac8b2ed75affb72937
zakaria.elhatimi <zakaria.elhatimi@etu.uae.ac.ma>
2025-03-10 23:36:43 +0000

    Default

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,7 +5,7 @@
   channel = "stable-24.05"; # or "unstable"
 
   # Use https://search.nixos.org/packages to find packages
-  home.packages = with pkgs; [
+    packages = [
     pkgs.sudo
     pkgs.python3
     pkgs.python311Packages.pip
