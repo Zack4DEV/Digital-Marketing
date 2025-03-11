168f7eb35ed83c5b43c1d578bc8a15deb31e49f7
zakaria.elhatimi <zakaria.elhatimi@etu.uae.ac.ma>
2025-03-11 22:37:54 +0000

    Pyproject.toml

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,28 +2,26 @@
 # see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
+  channel = "stable-24.11"; # or "unstable"
 
   # Use https://search.nixos.org/packages to find packages
-    packages = [
-    pkgs.sudo
-    pkgs.python3
-    pkgs.python311Packages.pip
-  #  pkgs.python311Packages.fastapi
-  #  pkgs.python311Packages.uvicorn
-    pkgs.pipenv
-    pkgs.nodejs_20
-    pkgs.nodePackages.nodemon
-    pkgs.gh
-    pkgs.git
-    pkgs.curl
-  ];
+  packages = [
+      pkgs.python3
+      pkgs.nodejs_23
+      pkgs.sqlite3
+      pkgs.go
+      pkgs.php
 
+      pkgs.sudo
+    #  pkgs.gh
+    #  pkgs.git
+  ];
+  
   # Sets environment variables in the workspace
   env = {};
    idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [ "ms-python.python" "rangav.vscode-thunder-client" ];
+    extensions = [ "ms-python.python" ];
     workspace = {
       # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
